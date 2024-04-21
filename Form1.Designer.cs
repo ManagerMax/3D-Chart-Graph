bb20d76c70f806f951239322f421aaf963058594
TheMaxxxman <bus123azs@bk.ru>
2024-04-21 14:53:14 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace _3D_Chart_Graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formula = new System.Windows.Forms.Button();
+            this.graph3D1 = new Plot3D.Graph3D();
+            this.show_all = new System.Windows.Forms.Button();
+            this.surface = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // formula
+            // 
+            this.formula.Location = new System.Drawing.Point(48, 20);
+            this.formula.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.formula.Name = "formula";
+            this.formula.Size = new System.Drawing.Size(112, 35);
+            this.formula.TabIndex = 1;
+            this.formula.Text = "Formula";
+            this.formula.UseVisualStyleBackColor = true;
+            this.formula.Click += new System.EventHandler(this.formula_Click);
+            // 
+            // graph3D1
+            // 
+            this.graph3D1.AxisX_Color = System.Drawing.Color.DarkBlue;
+            this.graph3D1.AxisX_Legend = null;
+            this.graph3D1.AxisY_Color = System.Drawing.Color.DarkGreen;
+            this.graph3D1.AxisY_Legend = null;
+            this.graph3D1.AxisZ_Color = System.Drawing.Color.DarkRed;
+            this.graph3D1.AxisZ_Legend = null;
+            this.graph3D1.BackColor = System.Drawing.Color.White;
+            this.graph3D1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.graph3D1.Location = new System.Drawing.Point(18, 88);
+            this.graph3D1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.graph3D1.Name = "graph3D1";
+            this.graph3D1.PolygonLineColor = System.Drawing.Color.Black;
+            this.graph3D1.Raster = Plot3D.Graph3D.eRaster.Off;
+            this.graph3D1.Size = new System.Drawing.Size(882, 625);
+            this.graph3D1.TabIndex = 0;
+            this.graph3D1.TopLegendColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(150)))));
+            // 
+            // show_all
+            // 
+            this.show_all.Location = new System.Drawing.Point(686, 20);
+            this.show_all.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.show_all.Name = "show_all";
+            this.show_all.Size = new System.Drawing.Size(112, 35);
+            this.show_all.TabIndex = 2;
+            this.show_all.Text = "Show All";
+            this.show_all.UseVisualStyleBackColor = true;
+            this.show_all.Click += new System.EventHandler(this.show_all_Click);
+            // 
+            // surface
+            // 
+            this.surface.Location = new System.Drawing.Point(242, 18);
+            this.surface.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.surface.Name = "surface";
+            this.surface.Size = new System.Drawing.Size(112, 35);
+            this.surface.TabIndex = 3;
+            this.surface.Text = "Sorface";
+            this.surface.UseVisualStyleBackColor = true;
+            //this.surface.Click += new System.EventHandler(this.surface_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(460, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 731);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.surface);
+            this.Controls.Add(this.show_all);
+            this.Controls.Add(this.formula);
+            this.Controls.Add(this.graph3D1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Plot3D.Graph3D graph3D1;
+        private System.Windows.Forms.Button formula;
+        private System.Windows.Forms.Button show_all;
+        private System.Windows.Forms.Button surface;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+//сторонние
+using delRendererFunction = Plot3D.Graph3D.delRendererFunction;
+using eRaster = Plot3D.Graph3D.eRaster;
+using eNormalize = Plot3D.Graph3D.eNormalize;
+using eSchema = Plot3D.ColorSchema.eSchema;
+
+namespace _3D_Chart_Graph
+{
+    public partial class Form1 : Form
+    {
+        double a = 0;
+        double b = 1;
+        double ep = 0.0001;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Блок с вычислением
+        double Func(double x, double y)
+        {
+            return (Math.Exp(x) + y) / (1 + x);
+        }
+        static double fi1(double x)
+        {
+            return Math.Log(1 + x);
+        }
+        static double fi2(double x)
+        {
+            return 1 + 2 * Math.Log(1 + x);
+        }
+        public double X(double u)
+        {
+            return a + u * (b - a);
+        }
+        double Y(double u, double v)
+        {
+            return fi1(X(u)) + v * (fi2(X(u)) - fi1(X(u)));
+        }
+        double Jacob(double Xn)
+        {
+            return (fi2(X(Xn)) - fi1(X(Xn)))* (b - a);
+        }
+
+        double IntegralFinder(int nX, int nY)
+        {
+            double Yn, Xn, sum = 0, hx = 1 / nX, hy = 1 / nY, finiteCell = hx * hy;
+
+            for (double u = 0; u < 1; u += hx)
+            {
+                for (double v = 0; v < 1; v += hy)
+                {
+                    Xn = u + hx / 2;
+                    Yn = v + hy / 2;
+                    double Jacobian = Math.Abs(Jacob(Xn));
+                    sum += Func(Xn, Yn) * Jacobian;
+                }
+            }
+            return sum * finiteCell;
+        }
+        public double RungeRule(int nx, int ny)
+        {
+            double Integral1, Integral2;
+            do
+            {
+                Integral1 = IntegralFinder(nx, ny);
+                nx *= 2;
+                ny *= 2;
+                Integral2 = IntegralFinder(nx, ny);
+            } while (Math.Abs((Integral2 - Integral1) / 3) > ep);
+            return Integral2 + (Integral2 - Integral1) / 3;
+        }
+        #endregion
+
+
+        private void show_all_Click(object sender, EventArgs e)
+        {
+            new Plot3D.Graph3DMainForm().ShowDialog();
+        }
+ 
+
+        private void formula_Click(object sender, EventArgs e)
+        {
+            SetFormula("(exp(x) + y) / (1 + x)");
+            graph3D1.Raster = (eRaster)1;
+            Color[] c_Colors = Plot3D.ColorSchema.GetSchema((eSchema)1);
+            graph3D1.SetColorScheme(c_Colors, 3);
+        }
+        private void SetFormula(string s_Formula)
+        {
+            try
+            {
+                delRendererFunction f_Function = Plot3D.FunctionCompiler.Compile(s_Formula);
+
+                // IMPORTANT: Normalize maintainig the relation between X,Y,Z values otherwise the function will be distorted.
+                int nx = int.Parse(textBox1.Text);
+                int ny = nx;
+
+                for (int i = 0; i < nx; i++)
+                {
+                    for (int j = 0; j < ny; j++)
+                    {
+                        double x = X((double)i / nx);
+                        double y = Y((double)i / nx, (double)j / ny);
+                        double z = Func(x, y);
+                        //nx = 20 .. 100
+                        // Добавляем точку на график
+                        graph3D1.SetFunction(f_Function, new PointF(-5, -5), new PointF(5, 5), z, eNormalize.MaintainXYZ);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }   
+    }
+}
